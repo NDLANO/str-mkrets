4970d5c2c71f16ca6694d316933fd48d00046aa0
janlindso <jan.frode.lindso@gmail.com>
2023-12-11 13:20:31 +0100

    Extracted from H5P iframe embedder

---
+++ static/nb-NO.js
@@ -0,0 +1,20 @@
+﻿window.elektrolyseDictionary = {
+  general: {
+    heading: 'Strømkabler',
+    powersource: 'Strømkilde',
+    cable: 'Kabel',
+    crossSection: 'Ledertverrsnitt',
+    resistivity: 'Resistivitet',
+    length: 'Lengde',
+    consumer: 'Forbruker',
+    current: 'Strøm',
+    calculation: 'Utregning',
+    warning:
+      'Pass på at kabelen er tilstrekkelig dimensjonert! En for tynn leder vil resultere i varmgang og fare for brann.',
+    meter: 'meter',
+  },
+  illustrasjon: {
+    sikringsskap: 'Sikringsskap',
+    ovn: 'Ovn',
+  },
+};
---
+++ static/nn-NO.js
@@ -0,0 +1,20 @@
+﻿window.elektrolyseDictionary = {
+  general: {
+    heading: 'Straumkablar',
+    powersource: 'Straumkjelde',
+    cable: 'Kabel',
+    crossSection: 'Leiartverrsnitt',
+    resistivity: 'Resistivitet',
+    length: 'Lengd',
+    consumer: 'Forbrukar',
+    current: 'Straum',
+    calculation: 'Utrekning',
+    warning:
+      'Pass på at kabelen er tilstrekkeleg dimensjonert! Ein for tynn leiar vil resultere i varmgang og fare for brann.',
+    meter: 'meter',
+  },
+  illustrasjon: {
+    sikringsskap: 'Sikringsskap',
+    ovn: 'Omn',
+  },
+};
